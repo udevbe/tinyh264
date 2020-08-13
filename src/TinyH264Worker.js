1bf912eb78ffad2e77ad4bfe012e96de8d12d2f7
erik <bleh>
2020-08-13 22:05:55 +0200

    remove babel runtime requirement

--- src/TinyH264Worker.js
+++ src/TinyH264Worker.js
@@ -21,44 +21,44 @@ function loadNativeModule (module) {
   })
 }
 
-async function init () {
+function init () {
   const tinyH264 = TinyH264({ locateFile: path => assets[path] })
-  await loadNativeModule(tinyH264)
+  return loadNativeModule(tinyH264).then(() => {
+    self.addEventListener('message', (e) => {
+      const message = e.data
+      const renderStateId = message.renderStateId
+      const messageType = message.type
+      switch (messageType) {
+        case 'decode': {
+          let decoder = h264Decoders[renderStateId]
+          if (!decoder) {
+            decoder = new TinyH264Decoder(tinyH264, (output, width, height) => {
+              postMessage({
+                type: 'pictureReady',
+                width: width,
+                height: height,
+                renderStateId: renderStateId,
+                data: output.buffer
+              }, [output.buffer])
+            })
+            h264Decoders[renderStateId] = decoder
+          }
+          decoder.decode(new Uint8Array(message.data, message.offset, message.length))
+          break
+        }
+        case 'release': {
+          const decoder = h264Decoders[renderStateId]
+          if (decoder) {
+            decoder.release()
+            delete h264Decoders[renderStateId]
+          }
+          break
+        }
+      }
+    })
 
-  self.addEventListener('message', (e) => {
-    const message = e.data
-    const renderStateId = message.renderStateId
-    const messageType = message.type
-    switch (messageType) {
-      case 'decode': {
-        let decoder = h264Decoders[renderStateId]
-        if (!decoder) {
-          decoder = new TinyH264Decoder(tinyH264, (output, width, height) => {
-            postMessage({
-              type: 'pictureReady',
-              width: width,
-              height: height,
-              renderStateId: renderStateId,
-              data: output.buffer
-            }, [output.buffer])
-          })
-          h264Decoders[renderStateId] = decoder
-        }
-        decoder.decode(new Uint8Array(message.data, message.offset, message.length))
-        break
-      }
-      case 'release': {
-        const decoder = h264Decoders[renderStateId]
-        if (decoder) {
-          decoder.release()
-          delete h264Decoders[renderStateId]
-        }
-        break
-      }
-    }
+    self.postMessage({ 'type': 'decoderReady' })
   })
-
-  self.postMessage({ 'type': 'decoderReady' })
 }
 
 export {
